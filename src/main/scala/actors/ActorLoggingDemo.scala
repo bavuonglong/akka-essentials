b522539aaa238911b05e518534d714fb8fb88a12
Cuong Ngo <bavuonglong93@gmail.com>
2020-02-24 10:28:22 +0100

    Actor logging

---
+++ src/main/scala/actors/ActorLoggingDemo.scala
@@ -0,0 +1,27 @@
+package actors
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.event.Logging
+
+object ActorLoggingDemo extends App {
+
+  class SimpleActorWithExplicitLogger extends Actor {
+    val logger = Logging(context.system, this)
+
+    override def receive: Receive = {
+      case message => logger.info(message.toString)
+    }
+  }
+
+  val system = ActorSystem("LoggingDemo")
+  val actor = system.actorOf(Props[SimpleActorWithExplicitLogger])
+
+  actor ! "Logging a simple message"
+
+
+  class ActorWithLogging extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
+}
